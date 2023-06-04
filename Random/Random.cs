037ad2b2f2423f30ed4cc02c41e872f15810bd58
Garrettdw304 <garrettdw14497@outlook.com>
2023-06-04 11:38:09 -0400

    Random updated for code reuse

--- Random/Random.cs
+++ Random/Random.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 // higher weight -> rarer
@@ -11,6 +12,8 @@ namespace GLIB.Random
 		public delegate int GetWeight<T>(T element);
 		public delegate int RandomProvider(int minInclusive, int maxExclusive);
 
+		public static readonly RandomProvider DEFAULT_RANDOM_PROVIDER = (a, b) => new System.Random((int)DateTime.Now.Ticks).Next(a, b);
+
 		/// <summary>
 		/// Returns a random element from the collection, chosen using a weighted system.
 		/// <para/>Higher weight -> less common
@@ -20,23 +23,7 @@ namespace GLIB.Random
 		/// <returns>The element that was chosen from elements.</returns>
 		public static T GetRandom<T>(ICollection<T> elements) where T : IWeighted
 		{
-			int totalWeight = 0;
-			foreach (T element in elements)
-			{
-				totalWeight += element.GetWeight();
-			}
-
-			int roll = new System.Random((int)DateTime.Now.Ticks).Next(0, totalWeight);
-
-			foreach (T element in elements)
-			{
-				roll -= element.GetWeight();
-
-				if (roll < 0)
-					return element;
-			}
-
-			return elements.First();
+			return GetRandom(elements, x => x.GetWeight(), DEFAULT_RANDOM_PROVIDER);
 		}
 
 		/// <summary>
@@ -48,23 +35,7 @@ namespace GLIB.Random
 		/// <returns>The element that was chosen from elements.</returns>
 		public static T GetRandom<T>(ICollection<T> elements, GetWeight<T> weightGetter)
 		{
-			int totalWeight = 0;
-			foreach (T element in elements)
-			{
-				totalWeight += weightGetter(element);
-			}
-
-			int roll = new System.Random((int)DateTime.Now.Ticks).Next(0, totalWeight);
-
-			foreach (T element in elements)
-			{
-				roll -= weightGetter(element);
-
-				if (roll < 0)
-					return element;
-			}
-
-			return elements.First();
+			return GetRandom(elements, weightGetter, DEFAULT_RANDOM_PROVIDER);
 		}
 
 		/// <summary>
@@ -77,23 +48,7 @@ namespace GLIB.Random
 		/// <returns>The element that was chosen from elements.</returns>
 		public static T GetRandom<T>(ICollection<T> elements, RandomProvider randomProvider) where T : IWeighted
 		{
-			int totalWeight = 0;
-			foreach (T element in elements)
-			{
-				totalWeight += element.GetWeight();
-			}
-
-			int roll = randomProvider(0, totalWeight);
-
-			foreach (T element in elements)
-			{
-				roll -= element.GetWeight();
-
-				if (roll < 0)
-					return element;
-			}
-
-			return elements.First();
+			return GetRandom(elements, x => x.GetWeight(), randomProvider);
 		}
 
 		/// <summary>
@@ -106,11 +61,7 @@ namespace GLIB.Random
 		/// <returns>The element that was chosen from elements.</returns>
 		public static T GetRandom<T>(ICollection<T> elements, GetWeight<T> weightGetter, RandomProvider randomProvider)
 		{
-			int totalWeight = 0;
-			foreach (T element in elements)
-			{
-				totalWeight += weightGetter(element);
-			}
+			int totalWeight = GetTotalWeight(elements, weightGetter);
 
 			int roll = randomProvider(0, totalWeight);
 
@@ -123,6 +74,15 @@ namespace GLIB.Random
 			}
 
 			return elements.First();
+		}
+
+		private static int GetTotalWeight<T>(ICollection<T> collection, GetWeight<T> weightGetter)
+		{
+			int totalWeight = 0;
+			foreach (T element in collection)
+				totalWeight += weightGetter(element);
+
+			return totalWeight;
 		}
 	}
 }
