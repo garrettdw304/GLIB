196e7a60fd2d0918aff85102690b060d5b68c508
Garrettdw304 <garrettdw14497@outlook.com>
2023-11-29 18:18:42 -0500

    Documented members of Random

--- Random/Random.cs
+++ Random/Random.cs
@@ -3,10 +3,39 @@
 	// higher weight -> rarer
 	public class Random
 	{
+		/// <summary>
+		/// A method that gets the weight of the passed element to be used when selecting a random weighted element.
+		/// <para/>higher weight -> less common
+		/// </summary>
+		/// <returns>An integer representing the weight of the passed element.</returns>
 		public delegate int GetWeight<T>(T element);
+		/// <summary>
+		/// Provides random numbers. Useful if the built in random provider (System.Random) does not meet your needs.
+		/// <para/>This functionallity was included because the Unity game engine uses an earlier version of the .NET runtime than this project is developed for, which was causing issues when using System.Random.
+		/// </summary>
+		/// <param name="minInclusive">The minimum value to be generated, inclusive.</param>
+		/// <param name="maxExclusive">The maximum value to be generated, exclusive.</param>
+		/// <returns>A random number between minInclusive and maxExclusive.</returns>
 		public delegate int RandomProvider(int minInclusive, int maxExclusive);
 
-		private static readonly RandomProvider DEFAULT_RANDOM_PROVIDER = (a, b) => new System.Random((int)DateTime.Now.Ticks).Next(a, b);
+		private static readonly RandomProvider DEFAULT_RANDOM_PROVIDER = System.Random.Shared.Next;
+
+		/// <summary>
+		/// Returns a random element from the collection, where every element has equal chance of being picked.
+		/// </summary>
+		public static T GetRandomUnweighted<T>(ICollection<T> elements)
+		{
+			return GetRandom(elements, x => 1);
+		}
+
+        /// <summary>
+        /// Returns a random element from the collection, where every element has equal chance of being picked.
+        /// <para/>This version of GetRandomUnweighted allows the mode of getting random values to be provided.
+        /// </summary>
+        public static T GetRandomUnweighted<T>(ICollection<T> elements, RandomProvider randomProvider)
+		{
+			return GetRandom(elements, x => 1, randomProvider);
+		}
 
 		/// <summary>
 		/// Returns a random element from the collection, chosen using a weighted system.
