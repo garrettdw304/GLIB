db7d1e063cc2110a8eb8e91681d6ec42e4a6d152
Garrettdw304 <garrettdw14497@outlook.com>
2023-11-29 17:59:00 -0500

    Documented members of Slot

--- Inventory/Slot.cs
+++ Inventory/Slot.cs
@@ -2,6 +2,9 @@
 
 namespace GLIB.Inventory
 {
+	/// <summary>
+	/// A 2 dimensional slot structure. Stores x and y values for accessing elements of an Inventory.
+	/// </summary>
 	public struct Slot
 	{
 		public int x;
@@ -13,18 +16,27 @@ namespace GLIB.Inventory
 			this.y = y;
 		}
 
+        /// <summary>
+        /// Parses a slot from a string, where a space seperates the x and y values used to construct the slot.
+        /// </summary>
 		public static Slot Parse(string str)
 		{
 			string[] strs = str.Split(' ');
 			return new Slot(int.Parse(strs[0]), int.Parse(strs[1]));
 		}
 
-		public static bool operator ==(Slot left, Slot right)
+        /// <summary>
+        /// Returns true if the left hand side's x and y match the right hand side's x and y, respectively. Otherwise false.
+        /// </summary>
+        public static bool operator ==(Slot left, Slot right)
 		{
 			return left.x == right.x && left.y == right.y;
 		}
 
-		public static bool operator !=(Slot left, Slot right)
+        /// <summary>
+        /// Returns true if the left hand side's x or y does not match the right hand side's x or y, respectively. Otherwise false.
+        /// </summary>
+        public static bool operator !=(Slot left, Slot right)
 		{
 			return left.x != right.x || left.y != right.y;
 		}
@@ -39,6 +51,10 @@ namespace GLIB.Inventory
             return base.GetHashCode();
         }
 
+        /// <summary>
+        /// Returns a string representation of this slot, including the value of both dimensions.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
