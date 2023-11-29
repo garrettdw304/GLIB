8fada0ce70793147149bd379a4e1deb4ae67d52b
Garrettdw304 <garrettdw14497@outlook.com>
2023-11-29 18:25:03 -0500

    Documented IWeighted

--- Random/IWeighted.cs
+++ Random/IWeighted.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace GLIB.Random
 {
+	/// <summary>
+	/// An element who has a weight associated with it. Used for weighted loot systems and the like for selecting random elements who have different chances of being picked.
+	/// <para/> higher weight -> less common
+	/// </summary>
 	public interface IWeighted
 	{
+		/// <summary>
+		/// Gets the weight of this object.
+		/// </summary>
 		public int GetWeight();
 	}
 }
